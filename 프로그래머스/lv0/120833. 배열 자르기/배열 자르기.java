224d98562b57331b6dcc5b7a50489cdda66e1a51
Park Yoon A <128116438+yoonaori@users.noreply.github.com>
2023-04-11 17:43:17 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = {};
+        answer = new int[num2-num1+1];
+        
+        for(int i=0; i<answer.length; i++){
+            answer[i] = numbers[num1];
+            num1++;
+        }
+    
+        return answer;
+    }
+}
