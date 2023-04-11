cee59d855469bf624424fb51dbabc4967c79a946
Park Yoon A <128116438+yoonaori@users.noreply.github.com>
2023-04-11 18:22:18 +0900

    Update 진료 순서 정하기.java

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -3,12 +3,13 @@ class Solution {
         int[] answer = {};
         answer = new int[emergency.length];
         
-        for(int i=0; i<emergency.length; i++){
-            for(int j=0; j<emergency.length; j++){
+        for(int i=0; i<emergency.length; i++){  // 응급도 숫자를 각각 비교하여
+            for(int j=0; j<emergency.length; j++){  // 숫자가 작으면 진료 순서가 높아지도록(후순위)
                 if(emergency[i] < emergency[j]){
                     answer[i]++;
                 }
             }
+            // 따로 초기화를 하지않아 디폴트가 0이므로 1 증가시켜줌 (제일 빠른 진료순서가 0이 아닌 1이도록)
             answer[i]++;
         }
         
