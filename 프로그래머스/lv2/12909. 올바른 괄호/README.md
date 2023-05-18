c83921cee678c6efb9fe8004eef5c5014633dbd1
Park Yoon A <128116438+yoonaori@users.noreply.github.com>
2023-05-18 09:55:09 +0900

    [level 2] Title: 올바른 괄호, Time: 18.15 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/README.md
+++ 프로그래머스/lv2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909#qna) 
 
 ### 성능 요약
 
-메모리: 53 MB, 시간: 19.12 ms
+메모리: 52.9 MB, 시간: 18.15 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -14,15 +14,14 @@ class Solution {
             if(s.charAt(i) == '('){
                 stack.push('(');  // 스택 값 추가
             } else {
-                if(!stack.isEmpty()){
+                if(!stack.empty()){
                     stack.pop(); // 스택 값 제거
                 }
-               
             }
         }
         
         // s가 올바른 괄호문이라면 스택이 비어있어야 함 
-        if(stack.isEmpty()){
+        if(stack.empty()){
             answer = true;
         } else {
             answer = false;
