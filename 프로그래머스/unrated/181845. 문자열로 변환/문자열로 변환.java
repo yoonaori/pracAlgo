169703ac9eec28eaae3cd8eca0aca6c4c4d84174
Park Yoon A <128116438+yoonaori@users.noreply.github.com>
2023-08-19 20:32:34 +0900

    [unrated] Title: 문자열로 변환, Time: 0.04 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        // answer = n + "";
+        // answer = Integer.toString(n);
+        answer = String.valueOf(n);
+        return answer;
+    }
+}
