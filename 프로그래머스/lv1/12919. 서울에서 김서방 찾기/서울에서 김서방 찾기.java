94faac06c71c095cbafe59d1c98c77472f90bd11
Park Yoon A <128116438+yoonaori@users.noreply.github.com>
2023-08-19 19:17:35 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 13.38 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int index = 0;
+        
+        for(String name : seoul){
+            if(name.equals("Kim")){
+                answer = "김서방은 " + index + "에 있다";
+                break;
+            }
+            index++;
+        }
+        return answer;
+    }
+}
