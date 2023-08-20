f16f62da75b4e8d2fca7912446e773519353f134
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2023-08-20 10:39:23 +0900

    [unrated] Title: 정수 찾기, Time: 0.01 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        
+        for(int num : num_list){
+            if(num == n){
+                answer = 1;
+                break;
+            } else{
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
