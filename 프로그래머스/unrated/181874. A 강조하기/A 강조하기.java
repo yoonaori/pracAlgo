59b863252dedb6b6683c951f1bc770f39654324d
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2023-08-23 22:02:56 +0900

    [unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toLowerCase();
+        answer = answer.replace("a", "A");
+        return answer;
+    }
+}
