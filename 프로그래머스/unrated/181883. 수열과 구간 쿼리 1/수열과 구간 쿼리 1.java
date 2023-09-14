a9d3a5f1e73fc9840710686cf77ad61079a9af28
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2023-09-14 21:55:46 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 1.60 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        
+        for(int[] query : queries){
+            for(int i=query[0]; i<query[1]+1; i++){
+                arr[i]++;
+            }
+        }
+        answer = arr;
+        return answer;
+    }
+}
