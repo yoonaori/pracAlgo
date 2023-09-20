f9f547043134a1c9d32089f1102ca2f4c3c059b1
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2023-09-20 22:34:25 +0900

    [level 0] Title: 7의 개수, Time: 1.35 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String str = "";
+        
+        for(int i : array){
+            str += Integer.toString(i);
+        }
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == '7'){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
