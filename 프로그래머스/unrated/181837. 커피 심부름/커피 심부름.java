2bb3eca8de2e7d9906b182f273160736c5790169
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2023-10-01 23:10:47 +0900

    [unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for(String o : order){
+            if(o.contains("americano")){
+                answer += 4500;
+            } else if(o.contains("cafelatte")){
+                answer += 5000;
+            } else if(o.contains("anything")){
+                answer += 4500;
+            }
+        }
+        return answer;
+    }
+}
