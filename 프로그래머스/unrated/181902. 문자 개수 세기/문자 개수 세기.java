2229369d42b34be478ad92c5bf034263269b98bf
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2023-10-09 23:18:39 +0900

    Update 문자 개수 세기.java

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -5,9 +5,11 @@ class Solution {
         
         for(int i=0; i<my_string.length(); i++){
             char ch = my_string.charAt(i);
-            
+
+            // 대문자인 경우
             if(ch>='A' && ch<='Z'){
                 answer[ch - 'A']++;
+            // 소문자인 경우
             } else if(ch>='a' && ch<='z'){
                 answer[26 + ch - 'a']++;
             }
