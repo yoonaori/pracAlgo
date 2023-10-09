ff730745eee00a465c8479779c51f0681bd612dd
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2023-10-09 23:01:01 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = {};
+        answer = new int[52];
+        
+        for(int i=0; i<my_string.length(); i++){
+            char ch = my_string.charAt(i);
+            
+            if(ch>='A' && ch<='Z'){
+                answer[ch - 'A']++;
+            } else if(ch>='a' && ch<='z'){
+                answer[26 + ch - 'a']++;
+            }
+        }
+        return answer;
+    }
+}
