911fec21b494c68c53a792d93973785389cdaede
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2024-01-19 22:12:48 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 12516 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for(int num : arr){
+            System.out.println(num);
+        }
+    }
+}
