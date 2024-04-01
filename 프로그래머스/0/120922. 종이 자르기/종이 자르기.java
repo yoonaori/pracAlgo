ccc840efcb37b89db5c6d29689c06c9eb7238014
YoonA.Park <128116438+yoonaori@users.noreply.github.com>
2024-04-01 23:09:02 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int M, int N) {
+        int answer = 0;
+        
+        answer += M - 1;
+        answer += (N - 1) * M;
+        
+        return answer;
+    }
+}
